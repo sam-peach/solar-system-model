f1334240d8466c9a313c59dde2ec29099f53275d
sam-peach <sampeachart@gmail.com>
2019-06-04 14:18:09 -0400

    upload

--- src/p5/planetConstructor.js
+++ src/p5/planetConstructor.js
@@ -1,14 +1,30 @@
 export default class Planet {
   constructor(p, diameter, distanceFromSun, scale) {
-    this.diameter = diameter * scale;
-    this.x = p.width / 2 - distanceFromSun * scale;
-    this.y = p.height / 2 - distanceFromSun * scale;
+    this.distanceFromSun = distanceFromSun / 100;
+    this.scale = scale;
+    this.diameter = (diameter / 100) * scale;
+    this.x = p.width / 2 - this.distanceFromSun * scale;
+    this.y = p.height / 2;
+    this.orbitRing(p);
     this.draw(p);
   }
 
   draw(p) {
+    p.noStroke();
     p.fill(255, 0, 0);
     p.ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+
+  orbitRing(p) {
+    p.noFill();
+    p.stroke(255);
+    p.strokeWeight(1);
+    p.ellipse(
+      p.width / 2,
+      p.height / 2,
+      this.distanceFromSun * this.scale * 2,
+      this.distanceFromSun * this.scale * 2
+    );
   }
 }
 
--- src/p5/sketch.js
+++ src/p5/sketch.js
@@ -1,10 +1,7 @@
-// import earth from "./earth";
-// import sun from "./sun";
 import Planet from "./planetConstructor";
-import { yieldExpression } from "@babel/types";
-
+import orbirtRings from "./orbitRings";
 export default function sketch(p) {
-  let scale = 0.001;
+  let scale = 0.00001;
 
   p.setup = function() {
     p.pixelDensity(1);
@@ -13,18 +10,24 @@ export default function sketch(p) {
   let xOffest = 0;
   let yOffest = 0;
   p.draw = function() {
-    p.translate(xOffest, yOffest);
+    console.log(scale);
     p.background(0);
+    p.translate(xOffest, yOffest);
     p.noStroke();
     p.smooth();
     const sun = new Planet(p, 864340, 0, scale);
     const mercury = new Planet(p, 3031.9 * 100, 35980000, scale);
     const venus = new Planet(p, 7520.8 * 100, 67240000, scale);
     const earth = new Planet(p, 7917.5 * 100, 92960000, scale);
+    const mars = new Planet(p, 4212.3 * 100, 141600000, scale);
+    const jupiter = new Planet(p, 86881 * 100, 483800000, scale);
+    const saturn = new Planet(p, 72367 * 100, 890800000, scale);
+    const uranus = new Planet(p, 31518 * 100, 1790000000, scale);
+    const neptune = new Planet(p, 30599 * 100, 2793000000, scale);
   };
 
   p.mouseWheel = event => {
-    scale += event.delta * 0.0000001;
+    scale += event.delta * 0.00001;
   };
 
   p.mouseDragged = event => {
